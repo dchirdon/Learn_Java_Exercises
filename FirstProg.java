17b7b7ccc4a4de2b87ac8c372d5cd94f2fae10ed
dchirdon <31939964+dchirdon@users.noreply.github.com>
2017-09-13 21:55:17 -0400

    Add files via upload

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+  public static void main( String[] args ) {
+    System.out.println( "I, David Chirdon, am determined to learn how to code" );
+    System.out.println( "Today's date is 13SEP2017" );
+  }
+}
